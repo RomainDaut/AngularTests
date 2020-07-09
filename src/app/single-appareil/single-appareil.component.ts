5482ff54b16fc25737410bef6df2dff70add3a5a
Romain Dautel <romaindaut@freegypsies.com>
2020-07-09 11:49:04 +0100

    routing by ID

--- src/app/single-appareil/single-appareil.component.ts
+++ src/app/single-appareil/single-appareil.component.ts
@@ -9,13 +9,13 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class SingleAppareilComponent implements OnInit {
 
-  name: string = 'Appareil';
-  status: string = 'Status';
+  name = 'Appareil';
+  status = 'Status';
 
   constructor(private appareilService: AppareilService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['id'];
+    const id = this.route.snapshot.params.id;
     this.name = this.appareilService.getAppareilById(+id).name;
     this.status = this.appareilService.getAppareilById(+id).status;
 
